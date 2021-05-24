2f83a144de37ea4871325311852e0cc7e036c569
mgweaver <masongweaver@gmail.com>
2021-05-23 22:54:13 -0600

    reset commit history to remove sg token

---
+++ app.js
@@ -0,0 +1,94 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://devuser:1Ja3BwxKGHHyz2RI@cse341project1db.cwsqy.mongodb.net/cse341project1db?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth')
+
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+
+const corsOptions = {
+    origin: "https://cse341project1mweaver.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes)
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    MONGODB_URI, options
+  )
+  .then(result => {
+    console.log('Connected to mongodb')
+    app.listen(process.env.PORT || 5000)
+  })
+  .catch(err => {
+    console.log(err);
+  });
